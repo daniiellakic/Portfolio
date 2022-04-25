50b82b84ac466ce9cbdb3605be6c6b0566156ca9
Danijel Lakic <50274258+daniiellakic@users.noreply.github.com>
2022-04-25 15:41:12 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Portfolio
-My  portfolio website.
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/6e12def9-b8e4-4ed0-9057-ab9bada12a3e/deploy-status)](https://app.netlify.com/sites/danijellakic/deploys)
-
-
-![Screenshot 2022-04-06 at 14 38 39](https://user-images.githubusercontent.com/50274258/161976578-02aab565-db52-44a9-a628-340079b042da.png)
+My first portfolio inm HTML, CSS, JavaScript
--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-window.onscroll = function() {myFunction()};
-  
-    var navbar = document.getElementById( "navbar");
-    var sticky = navbar.offsetTop;
-  
-    /* Function to stick the nav bar */
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-        }
-        else {
-            navbar.classList.remove("sticky");
-        }
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+/* Function to stick the nav bar */
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
     }
+}
 
 
 
- 
-  //Toggle menu
- 
+
+//Toggle menu
+
 const icon = document.getElementsByClassName('icon')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
@@ -28,10 +27,11 @@ icon.addEventListener('click', () => {
 
 
 const icons = document.querySelectorAll('.icon');
-icons.forEach (icon => {  
-  icon.addEventListener('click', (event) => {
-    icon.classList.toggle("open");
-  });
+icons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        icon.classList.toggle("open");
+    });
 });
 
-
+//Get the video
+var video = document.getElementById("myVideo");
